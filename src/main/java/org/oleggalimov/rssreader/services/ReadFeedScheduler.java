f45c543aa07f4b579f3e5588b8cb555e2bc26d4a
oleg <o.d.galimov@yandex.ru>
2020-05-08 19:38:33 +0500

    readme fix

--- src/main/java/org/oleggalimov/rssreader/services/ReadFeedScheduler.java
+++ src/main/java/org/oleggalimov/rssreader/services/ReadFeedScheduler.java
@@ -4,14 +4,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.oleggalimov.rssreader.cg.CgRawDataConverter;
 import org.oleggalimov.rssreader.cg.CgRawDataLoader;
 import org.oleggalimov.rssreader.da.IFeedRecordsRepository;
+import org.oleggalimov.rssreader.dto.FeedRecord;
 import org.oleggalimov.rssreader.dto.request.ExtractingRule;
 import org.oleggalimov.rssreader.tasks.LoadFeedTask;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.*;
 
@@ -49,21 +53,26 @@ public class ReadFeedScheduler {
     @Scheduled(fixedRate = 180000)
     public void loadFeed() {
         log.info("Tasks: {}", feedMap.size());
-        Map<String, Future<Boolean>> futureList = new HashMap<>();
+        Map<String,  Future<List<FeedRecord>>> futureList = new HashMap<>();
         feedMap.forEach((url, extractingRule) -> {
-                    LoadFeedTask task = new LoadFeedTask(cgRawDataLoader, cgRawDataConverter, extractingRule, recordsRepository, url);
-                    Future<Boolean> future = executorService.submit(task);
+                    LoadFeedTask task = new LoadFeedTask(cgRawDataLoader, cgRawDataConverter, extractingRule, url);
+                    Future<List<FeedRecord>> future = executorService.submit(task);
                     futureList.put(url, future);
                 }
         );
-        futureList.forEach((url, booleanFuture) -> {
+        futureList.forEach((url, future) -> {
             try {
-                booleanFuture.get(160, TimeUnit.SECONDS);
+                List<FeedRecord> feedRecords = future.get(160, TimeUnit.SECONDS);
+                saveData(feedRecords);
             } catch (InterruptedException | ExecutionException | TimeoutException e) {
                 log.error("Error on task for url: {}", url);
             }
         });
         log.info("End of task: {}", feedMap.size());
-
+    }
+//    @Transactional
+    private void saveData(List<FeedRecord> records) {
+        log.info("Saving {} records", records.size());
+        recordsRepository.saveAll(records);
     }
 }
--- src/main/java/org/oleggalimov/rssreader/tasks/LoadFeedTask.java
+++ src/main/java/org/oleggalimov/rssreader/tasks/LoadFeedTask.java
@@ -11,6 +11,7 @@ import org.oleggalimov.rssreader.da.IFeedRecordsRepository;
 import org.oleggalimov.rssreader.dto.request.ExtractingRule;
 import org.oleggalimov.rssreader.dto.FeedRecord;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.Callable;
 
@@ -18,35 +19,32 @@ import java.util.concurrent.Callable;
 @Getter
 @Setter
 @AllArgsConstructor
-public class LoadFeedTask implements Callable<Boolean> {
+public class LoadFeedTask implements Callable< List <FeedRecord>> {
     private CgRawDataLoader loader;
     private CgRawDataConverter converter;
     private ExtractingRule rule;
-    private IFeedRecordsRepository recordsRepository;
     private String url;
 
     @Override
-    public Boolean call() {
+    public List <FeedRecord> call() {
         log.debug("Starting scheduled task");
         try{
             log.debug("Loading data from {}", url);
             Document document = loader.loadDataFromUrl(url);
             if (document == null) {
                 log.error("Empty data from raw document");
-                return Boolean.FALSE;
+                return Collections.emptyList();
             }
             log.debug("Converting raw data");
             List<FeedRecord> feedRecords = converter.convert(rule, document);
             if (feedRecords==null || feedRecords.isEmpty()) {
                 log.error("No data from document");
-                return Boolean.FALSE;
+                return Collections.emptyList();
             }
-            log.debug("Saving {} records", feedRecords.size());
-            recordsRepository.saveAll(feedRecords);
-            return Boolean.TRUE;
+           return feedRecords;
         } catch (Exception ex) {
             log.error("Exception in task for URL {}", url, ex);
-            return Boolean.FALSE;
+            return Collections.emptyList();
         }
     }
 }
